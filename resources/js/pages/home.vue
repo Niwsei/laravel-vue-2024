8ec721904e7cc3365ec438a92db1dff30add33be
Niwsei <loveonelove2004@gmail.com>
2024-02-19 13:28:21 +0700

    first commit

---
+++ resources/js/pages/home.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+<h1>Home Vue</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    },
+
+    data(){
+        return {
+
+        }
+    },
+    methods: {
+            
+        },
+
+        computed: {
+
+        },
+        watch: {
+
+        },
+
+        created: {
+
+        }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
